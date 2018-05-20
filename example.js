231a31bd3ecd32ef790919f5299081ac11c76435
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2018-05-20 20:24:00 +0100

    Fix example event binding

--- example.js
+++ example.js
@@ -2,38 +2,42 @@
 
 import SoundBox from "./soundbox.js";
 
-window.addEventListener("load", function(event) {
+window
+	.addEventListener("load", function(event) {
 	window.soundbox = new SoundBox();
 	soundbox.load("beep1", "examplesounds/beep1.wav");
 	soundbox.load("beep2", "examplesounds/beep2.wav");
 	soundbox.load("beep3", "examplesounds/beep3.wav");
-});
+	
+	document.getElementById("play_sound_1")
+		.addEventListener("click", function() {
+			soundbox.play("beep1");
+		});
 
-function play_sound_1()
-{
-	soundbox.play("beep1");
-}
-function play_sound_2()
-{
-	soundbox.play("beep2");
-}
-function play_sound_3()
-{
-	soundbox.play("beep3");
-}
+	document.getElementById("play_sound_2")
+		.addEventListener("click", function() {
+			soundbox.play("beep2");
+		});
 
-function play_multiple_sounds()
-{
-	soundbox.play("beep1", function() {
-		soundbox.play("beep2", function() {
+	document.getElementById("play_sound_3")
+		.addEventListener("click", function() {
 			soundbox.play("beep3");
 		});
-	});
-}
 
-function play_multiple_sounds_promise()
-{
-	window.soundbox.play("beep1")
-		.then(() => window.soundbox.play("beep2"))
-		.then(() => window.soundbox.play("beep3"));
-}
+	document.getElementById("play_multiple_sounds")
+		.addEventListener("click", function() {
+			soundbox.play("beep1", function() {
+				soundbox.play("beep2", function() {
+					soundbox.play("beep3");
+				});
+			});
+		});
+
+	document.getElementById("play_multiple_sounds_promise")
+		.addEventListener("click", function() {
+			window.soundbox.play("beep1")
+				.then(() => window.soundbox.play("beep2"))
+				.then(() => window.soundbox.play("beep3"));
+		});
+	
+});
